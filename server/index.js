1e22f8c4fb345bf39d8c6be7604dd62417f4b261
Gareth Rogers <garethdavisrogers@gmail.com>
2021-05-25 15:52:00 -0500

    react app set up but not functional

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.static("./client/public"));
+
+app.listen(port, () => {
+  console.log("node is listening");
+});
